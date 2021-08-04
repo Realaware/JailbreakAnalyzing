0aeef66c5410801df2da3bf19e2eb12d0abba200
RealReal <63380308+jiwonpaly@users.noreply.github.com>
2021-08-04 15:23:00 +0900

    Update Donut.lua

--- analyzing/Donut.lua
+++ analyzing/Donut.lua
@@ -1,5 +1,1 @@
-local func = require(game:GetService("ReplicatedStorage").Game.Item.Donut).InputBegan;
-local proto = debug.getproto(func, 1);
-local upv = debug.getupvalue(proto, 1);
-
-print(type(upv))
+-- not ready
